d13b1700dca55917a4bd0dc4cd342f255aa7bf79
Rishitha-2002 <96543017+Rishitha-2002@users.noreply.github.com>
2022-05-11 21:01:23 +0530

    Create circularLinkedList.java
    Circular Linked List

---
+++ circularLinkedList.java
@@ -0,0 +1,22 @@
+//link : https://practice.geeksforgeeks.org/problems/circular-linked-list/1
+
+
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	// Your code here
+	if(head==null)
+	 return true;
+	 
+	Node slow = head;
+	Node fast = head;
+	while(fast!=null && fast.next!=null){
+	    slow = slow.next;
+	    fast = fast.next.next;
+	    if(slow==fast)
+	     return true;
+	}
+	return false;
+    }
+}
